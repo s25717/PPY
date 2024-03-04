fa242e4f1395d57e3284e0d1e977cda0fdf1231e
Zhanel Nurkhabanova <s25717@pjwstk.edu.pl>
2024-03-04 12:32:19 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Prompt the user to enter their name
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+age = input("How old are you?: ")
+
+print("Hello,", name +" "+ surname + "!")
+print("I know your age, you are " + age + " years old!")
+
+def print_hi(name):olio;u;
+    # Use a breakpoint in the code line below to debug your script.
+    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
